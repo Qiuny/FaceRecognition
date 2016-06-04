c745deec5df648cb74468b4f8f976ae0b1df0328
Qiuny <qiu2458@163.com>
2016-06-04 11:54:06 +0800

    Initial Commit

---
+++ FaceRecognition/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FaceRecognition
+//
+//  Created by Joggy on 16/6/4.
+//  Copyright © 2016年 Joggy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
